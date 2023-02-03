2e534bf859b85d39b093a4a9775467e32ad5d0f4
Italo <italo.alves.jp@gmail.com>
2023-02-03 14:32:36 -0300

    fix: timestamp field missing

--- forecast_report/src/dao/weather_forecast_dao.py
+++ forecast_report/src/dao/weather_forecast_dao.py
@@ -3,6 +3,7 @@ from db.Db import Db
 class WeatherForecastDAO:
     def __init__(self):
        self.db = Db()
+        self.table_name = "weather_forecasts"
 
     def list(self):
-       return self.db.execute('SELECT * FROM users', None).fetchall()
+       return self.db.execute(f'SELECT * FROM `{self.table_name}`', None).fetchall()
